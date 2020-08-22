d0c3f92ca7abe579502b48852a32c3e5f712fa12
Oleksandr Kolomiichuk <alexkolom27@gmail.com>
2020-08-22 17:02:23 +0200

    Add the ability to create HTML blocks using form inputs

--- src/model.js
+++ src/model.js
@@ -4,22 +4,33 @@ import {
 	TextColumnsBlock,
 	TitleBlock
 } from './classes/blocks'
+import { css } from './utils'
 import image from './assets/travellers.png'
 
 export const model = [
 	new TitleBlock('Website constructor', {
 		tag: 'h2',
-		styles: 'text-align: center;'
+		styles: css({
+			'text-align': 'center'
+		})
 	}),
 	new TextBlock('Build sites with just a vanilla JavaScript', {
-		styles: 'text-align: center; font-weight: bold; color: #777;'
+		styles: css({
+			'text-align': 'center',
+			'font-weight': 'bold',
+			color: '#777'
+		})
 	}),
 	new ImageBlock(image, {
 		alt: 'my image',
-		styles: 'padding: 2rem 0; display: flex; justify-content: center;',
-		imageStyles: 'width: 140px; height: auto;'
+		styles: css({
+			padding: '2rem 0',
+			display: 'flex',
+			'justify-content': 'center'
+		}),
+		imageStyles: css({ width: '140px', height: 'auto' })
 	}),
 	new TextColumnsBlock(['1 text', '2 text', '3 text'], {
-		styles: 'text-align: center; border: 1px dashed #ddd;'
+		styles: css({ 'text-align': 'center', border: '1px dashed #ddd' })
 	})
 ]
